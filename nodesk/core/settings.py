0646cc8c464aad466832770ed6f34d4cacaa38fc
DankoCaboski <caboskidanko@gmail.com>
2025-09-03 22:45:20 -0300

    Teste: rollback def load_settings

--- nodesk/core/settings.py
+++ nodesk/core/settings.py
@@ -12,12 +12,8 @@ class Settings(BaseSettings):
     APP_ENVIRONMENT: Literal["development", "production", "testing"] = Field()
     APP_SECRET: SecretStr = Field()
 
-    BASE_DIR: ClassVar[str] = os.path.dirname(os.path.abspath(__file__))
-
     model_config = SettingsConfigDict(
-        env_file=os.path.join(BASE_DIR, "../../.env"),
-        env_file_encoding="utf-8",
-        case_sensitive=True
+        env_file=".env", env_file_encoding="utf-8", case_sensitive=True
     )
 
 
